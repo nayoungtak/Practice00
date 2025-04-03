4bf255499fd0821fbdcd617e762c1dcc587f0a0e
mirjam060518@gmail.com <mirjam060518@gmail.com>
2025-04-03 18:31:33 +0900

    2025.04.03

---
+++ Practice2/train01.c
@@ -0,0 +1,67 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	int d;
+	d = 0xabcd;
+	printf("%#X", d);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
